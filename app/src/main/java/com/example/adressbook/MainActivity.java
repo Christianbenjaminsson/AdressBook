679dda9dcdc6609793e2e6b1424e5cfc900f7c01
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-06-09 22:00:50 +0200

    Create MainActivity and method for splashscreen

--- app/src/main/java/com/example/adressbook/MainActivity.java
+++ app/src/main/java/com/example/adressbook/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.adressbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        splashScreen();
+    }
+
+    public void splashScreen () {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        }, 3000);
     }
 }
